3d855003eec89c66d13b0d0d74a681b17f889c8a
RomSantaRosa <mourarom@gmail.com>
2020-04-07 01:18:23 +0100

    create-structure and initialpage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Notification, Section } from 'rbx'
+import './App.scss'
+import HomeScreen from './screens/home'
+
+
+const App = () => (
+<>
+<HomeScreen/>
+</>
+)
+
+export default App;
